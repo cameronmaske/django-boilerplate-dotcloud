1d70009d3ebcf860e01557f4dac8eaef5f5b442a
Cameron Maske <cameronmaske@gmail.com>
2012-04-17 15:17:43 +0100

    Forgot to add in the directory

---
+++ {{ project_name }}/config/environments/production.py
@@ -0,0 +1,29 @@
+from ..settings import *
+
+#Small check to make sure we're on DotCloud. 
+#Interesting snippet. Keep in for the moment, will write a test for it in the future. 
+import getpass
+if getpass.getuser() == 'dotcloud': 
+    DOTCLOUD = True 
+
+
+DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',    
+            'NAME': 'template0', #Change this. 
+            'USER': env['DOTCLOUD_DB_SQL_LOGIN'],
+            'PASSWORD': env['DOTCLOUD_DB_SQL_PASSWORD'],
+            'HOST': env['DOTCLOUD_DB_SQL_HOST'],
+            'PORT': int(env['DOTCLOUD_DB_SQL_PORT']),
+    }
+}
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+# settings/local.py is ignored to allow for easy settings
+# overrides without affecting others
+try:
+    from local import *
+except ImportError:
+    pass
