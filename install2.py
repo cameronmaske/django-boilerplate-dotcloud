7f07bffc8c60b9d0daf2636db3b118fd9524a6d5
Cameron Maske <cameronmaske@gmail.com>
2012-07-23 00:09:32 +0100

    Major changes to how the project is setup with the virtualenv. Now should work. README now updated. Further todos outlined in install2.py

--- install2.py
+++ install2.py
@@ -37,32 +37,48 @@ def query_yes_no(question):
 		else:
 			sys.stdout.write("Please respond with 'Y' or 'N'\n")
 
+def virtualenv(command):
+	os.system(env_activate + '&&' + command)
+
 if __name__ == '__main__':
 	path = os.getcwd()
 	#We get the user to input the desired app name. 
 	print "Let's setup your app! "
-	app_name = raw_input("Enter the app's name.\n This will be shared with both DotCloud and Django.\n ")
-	
+	app_name = raw_input("Enter the app's name.\nThis will be shared with both DotCloud and Django.\n")
+	app_path = "%s/%s" % (path,app_name)
+
+	#A bit of fun. 
+	print "And so it shall be, that %s will be created!" % app_name
 	#Next, we setup the virtualenv. 
-	#Let's deactivate any virtualenv currently set. 
-	#os.system("deactivate")
 	print "Creating the app '%s' directory and virtual enviroment." % app_name
+	#Is there a way to share agurements when formatting strings?
 	os.system('virtualenv --python=python2.7 %s' % app_name)
-	print "Changing directory to %s/%s" % (path,app_name)
-	os.chdir("%s/%s/" % (path,app_name))
+
+	#Creating the enviroment path. 
+	#Need to create global variables (odd in python huh!).
+	#This is due to os.system creating a sub-shell each time it is called. 
+	#This may not be the ideal solution, but it works for now. 
+	
 	print "Activating the virtual enviroment."
-	os.system("source bin/activate")
+	global env_path 
+	env_path = "%s/bin/activate" % app_path
+	global env_activate 
+	env_activate = "source %s" % env_path
+
+	#Next, we cd into the project's folder. 
+	print "Changing directory to %s/%s" % (path,app_name)
+	os.chdir(app_path)
 
 	#Now we setup Django.
 	print "Installing Django, please hold."
-	os.system("pip install django")
+	virtualenv("pip install django")
 	#Note: At this point, we should check that the name is valid. https://github.com/django/django/blob/master/django/core/management/commands/startproject.py
 	print "Django has been installed."
 
 	#We create the template project for our django app. 
 	print "Creating a new django project boilerplate. Don't worry you can customize it soon enough. "
 	#Runs an uber startproject. Should access all files with the extension tag, and create the project within the current directory. 
-	os.system('django-admin.py startproject --template https://github.com/cameronmaske/django-boilerplate-dotcloud/zipball/master %s --extension py,md,conf,yml .' % app_name)
+	virtualenv('django-admin.py startproject --template https://github.com/cameronmaske/django-boilerplate-dotcloud/zipball/master %s --extension py,md,conf,yml .' % app_name)
 	
 
 	#Let's start the customization. 
@@ -104,6 +120,54 @@ if __name__ == '__main__':
 		pass
 		#replace_line_re("%s/config/settings.py", "#Beta waiting", "waiting")
 
+
+	#Finishing off the install. 
+	print "Removing redudant files used for install"
+	#Removes redudant files, such as our install script. 
+	os.system("rm install.py")
+	os.system("rm install2.py")
+
+	#Let's create a nice template README.md for the project. 
+	#In the future hopefully this will be more robust, include the project's name and written better. 
+	#For now it will do. 
+	print "Creating a fresh README.md"
+	file = open('README.md', 'w')
+	file.write(
+	"""
+	This is a main heading.
+	==============
+
+	This is a sub heading
+	--------------
+
+	*This is Italics*
+
+	**This is Bold**
+
+	- This is list item
+	- This is list item
+
+		An indent will end up as code
+
+	Not sure what to put in a README.md?
+	--------------
+	*[This may help](http://stackoverflow.com/questions/2304863/how-to-write-a-good-readme)*
+	"""
+	)
+	file.close()
+
+	#After the setup of the files, let's run the project. 
+	# - syncdb
+	# - migrate -all (south)
+	# - validate --settings=config.enviroment
+	# - runserver& (in the background)
+
+	#Finally, give the user the option to push to dotcloud. 
+	decision = query_yes_no("\nDo you want to push the app to DotCloud?")
+	# - create a new dotcloud app. 
+	# - push the app. 
+
+	# Fin! #
 
 
 
